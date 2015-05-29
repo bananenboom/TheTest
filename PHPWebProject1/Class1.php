bcca357a6e91cba58a512e22e3730a33d1b02c6c
bananenboom <peteroerlemans@hotmail.com>
2015-05-29 22:31:53 +0200

    Hello

---
+++ PHPWebProject1/Class1.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class1 short summary.
+ *
+ * Class1 description.
+ *
+ * @version 1.0
+ * @author Peter
+ */
+class Class1
+{
+    function Hello()
+    {
+        
+        return "Hello";
+    }
+}
